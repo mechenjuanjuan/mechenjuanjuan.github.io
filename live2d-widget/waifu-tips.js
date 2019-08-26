d03a16e27cb48891f3833e7a96df6a9d198d0845
cyp <chenyupeng99@163.com>
2019-08-26 12:02:36 +0800

    Site updated: 2019-08-26 12:02:35

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -54,7 +54,7 @@ function loadWidget(waifuPath, apiPath) {
 			});
 		});
 		var re = /x/;
-		console.log(re);
+		//console.log(re);
 		re.toString = () => {
 			showMessage("哈哈，你打开了控制台，是想要看看我的小秘密吗？", 6000, 9);
 			return "";
@@ -183,12 +183,12 @@ function loadWidget(waifuPath, apiPath) {
 		});
 	}
 	initModel();
-
+	//, console.log(`Live2D 模型 ${modelId}-${modelTexturesId} 加载完成`)
 	function loadModel(modelId, modelTexturesId) {
 		localStorage.setItem("modelId", modelId);
 		if (modelTexturesId === undefined) modelTexturesId = 0;
 		localStorage.setItem("modelTexturesId", modelTexturesId);
-		loadlive2d("live2d", `${apiPath}/get/?id=${modelId}-${modelTexturesId}`, console.log(`Live2D 模型 ${modelId}-${modelTexturesId} 加载完成`));
+		loadlive2d("live2d", `${apiPath}/get/?id=${modelId}-${modelTexturesId}`);
 	}
 
 	function loadRandModel() {
