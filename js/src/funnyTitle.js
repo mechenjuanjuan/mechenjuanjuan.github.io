85a6ff849aadd36099ed1fbe50c2f07e49b39879
chen <chenyupeng99@163.com>
2020-03-25 17:16:09 +0800

    Site updated: 2020-03-25 17:16:08

--- js/src/funnyTitle.js
+++ js/src/funnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/images/favicon.ico");
-         document.title = '(´⊙ω⊙`)别走我养你啊~(想得美)';
+         document.title = '(´⊙ω⊙`)网络崩溃了~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/images/favicon.ico");
-         document.title = ' (๑• . •๑) 人平安回来就好~(滑稽)' + OriginTitle;
+         document.title = ' (๑• . •๑) 诶,我又好了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
